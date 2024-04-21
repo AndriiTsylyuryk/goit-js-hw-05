aa1ab6298481a08c08ae674ed3874f5ef397b490
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-04-21 09:36:40 +0300

    after mentor obs

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,22 @@
 "use strict";
 
 class StringBuilder {
-  //                          .
+#value
   constructor(initialValue = "") {
-    this.value = initialValue;
+    this.#value = initialValue;
   }
-  getValue() {return this.value}
+  getValue() {return this.#value}
 
   padEnd(str) {
-    this.value += str
+    this.#value += str
   }
 
   padStart(str){
-this.value = str + this.value
+this.#value = str + this.#value
   }
 
   padBoth(str){
-    this.value = str + this.value + str
+    this.#value = str + this.#value + str
   }
 }
 
